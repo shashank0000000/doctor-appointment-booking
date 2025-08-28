6020be57a9eba11b8351caea711d6309300603fe
Shashank Shekhar <56396662+shashank0000000@users.noreply.github.com>
2025-08-28 16:27:18 +0530

    slight modif

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
         {/* ------- Copyright text ------- */}
         <div>
           <hr />
-          <p className='py-5 text-sm text-center'>Copyright © 2024 GreatStack - All Right Reserved.</p>
+          <p className='py-5 text-sm text-center'>Copyright © 2025 Shwshnk - All Right Reserved.</p>
         </div>
     </div>
   )
